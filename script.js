f411054c10ae5cd54113162092637a78dcd89480
ntf537 <159794676+ntf537@users.noreply.github.com>
2025-01-20 18:46:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+const myURL = "D:\to_do_listcards-data";
+
 const temp = document.getElementById("temp");
 const icon = document.getElementById("icon");
 const rate = document.getElementById("rate");
+
+const editButton = document.getElementById("edit-button");
+const addButton = document.getElementById("add-button");
+const saveButton = document.getElementById("save-button");
+
+const dialogAddWindow = document.getElementById("add-task-dialog");
+const inputTitle = document.getElementById("get-task-title");
+
 async function fetchWeatherData() {
   const weatherUrl =
     "https://api.openweathermap.org/data/2.5/weather?lat=53.55&lon=27.33&appid=0a7d636a97a3858fc7eeb7f659688885";
@@ -40,6 +50,31 @@ function renderRateData(data) {
 
   rate.textContent = drate;
 }
+
+function onAddClick() {
+  dialogAddWindow.showModal();
+}
+
+async function fetchAddData() {
+  localStorage.setItem(
+    "cards",
+    JSON.stringify([{ id: "1", title: inputTitle.value }])
+  );
+  for (let i = 0; i < JSON.parse(localStorage.getItem("cards")).length; i++) {
+    let { id: idValue, title: titleValue } = JSON.parse(
+      localStorage.getItem("cards")
+    )[i];
+    console.log("id: " + idValue + " title: " + titleValue);
+  }
+  dialogAddWindow.close();
+}
+
+function onSaveClick() {
+  fetchAddData();
+}
+
+addButton.addEventListener("click", onAddClick);
+saveButton.addEventListener("click", onSaveClick);
 (function () {
   fetchWeatherData();
   fetchRateData();
